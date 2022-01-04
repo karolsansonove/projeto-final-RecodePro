13571570da0e755344387bef6ffe54c942eba48b
Dyego Felipe de Oliveira <dyegooliveira08@gmail.com>
2022-01-04 18:13:36 -0300

    Script para inserção de produtos e artesaos no banco de dados

---
+++ Script_Artesaos_Produtos.sql
@@ -0,0 +1,27 @@
+select * from Produto;
+select * from Artesao;
+
+
+insert into Artesao values
+('Ubiraja Xamã','12345','ubirajaxama@recode.com.br'),
+('Thaynara Rudá','56789','Thaynararuda@recode.com.br'),
+('Raoni Porã','13467', 'raonipora@recode.com.br'),
+('Iara Nina','78945', 'iaranina@recode.com.br'),
+('Jurema Juraci','654321', 'juremajuraci@recode.com.br');
+
+
+insert into Produto values 
+('Brincos', 'Brincos indígenas Arara Canindé', 25.00, 15, 'https://img.elo7.com.br/product/main/3949E86/brinco-indigena-arara-caninde.jpg', 2),
+('Brincos', 'Brincos indígenas em miçangas', 17.90 ,20,'https://img.elo7.com.br/product/zoom/357033F/brinco-indigena-em-micangas-indigena.jpg',2),
+('Brincos', 'Brincos indígenas penas e sementes', 28.00, 10, 'https://img.elo7.com.br/product/main/357069E/brinco-indigena-de-penas-e-sementes-indigena.jpg',3),
+('Brincos', 'Brincos indígenas de penas', 26.50, 10, 'https://img.elo7.com.br/product/original/283DCED/brinco-apanhador-de-sonhos-pena-indigena.jpg',3),
+('Colar','Colar indínega em miçangas', 35.00, 15, 'https://img.elo7.com.br/product/zoom/35701D9/colar-indigena-em-micangas-colar.jpg',4),
+('Colar', 'Colar indígena aromatizador pessoal',49.50, 18, 'https://www.aromaterapiabysamia.com.br/image/cache/catalog/Ind%C3%ADgena1-1000x1000.jpeg', 2),
+('Colar', 'Colar indígena com discos de coco natual', 109.99,5, 'https://http2.mlstatic.com/D_NQ_NP_889492-MLB41801293895_052020-O.jpg',4),
+('Pulseira', 'Pulseira indígena larga laranja, branco e preto',159.99,10, 'https://images.tcdn.com.br/img/img_prod/752068/pulseira_indigena_larga_laranja_branco_e_preto_77_1_20200212192822.jpg',5),
+('Pulseira','Pulseira indígena natural semente de açaí', 145.00, 10, 'https://d2r9epyceweg5n.cloudfront.net/stores/536/128/products/rudraksha-e-acai1-d1728e9f57255b98dd15215810431903-1024-1024.jpg',5),
+('Pulseira', 'Pulseira de fibra natural e fio de cobre', 84.99,20, 'https://cdn.awsli.com.br/600x450/649/649036/produto/92793216394e7a7be9.jpg',3),
+('Cesto', 'Cesto indígena natural de palha 30 cm', 289.99,15,'https://d3ugyf2ht6aenh.cloudfront.net/stores/906/252/products/cesta-de-palha-indigena1-b529afef25323e7ea516068556417594-1024-1024.jpg',5),
+('Cesto', 'Cesto indígena natural de palha trançado', 495.99,15,'http://d3ugyf2ht6aenh.cloudfront.net/stores/001/615/145/products/14420853099-dsc1069-00d6732458de2f5dca16349152905821-640-0.jpg',2),
+('Bolsa', 'Bolsa indígena', 349.00, 25, 'https://d3g37he68niee0.cloudfront.net/SP/dafiti/2319597_01.jpg', 3),
+('Bolsa', 'Bolsa indígena', 99.99, 20, 'https://d3ugyf2ht6aenh.cloudfront.net/stores/805/692/products/pic_20200423_162322-44e5dbc098de692f8915876699030113-1024-1024.jpg',4);
