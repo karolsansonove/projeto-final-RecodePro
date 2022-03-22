b2868327292d07a51a0029487ed55ca420879f36
RobsonOverall <robsonjunoverall@gmail.com>
2022-03-21 22:19:40 -0300

    Nova Model ItemCarrinho e Pasta Suporte .vscode

---
+++ ProjetoFinal/Models/ItemCarrinho.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjetoFinal.Models
+{
+    public class ItemCarrinho
+    {
+        [Key]
+        public string IdItem { get; set; }
+        public string IdCarrinho { get; set; }
+        public int Quantidade { get; set; }
+        public System.DateTime DateCreated { get; set; }
+        public int ProdutoIdProduto { get; set; }
+        public virtual Produto Produto { get; set; }
+    }
+}
