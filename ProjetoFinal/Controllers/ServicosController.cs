cbbd8a8ad39f401d0314e867ce1916f27c09c133
RobsonOverall <robsonjunoverall@gmail.com>
2022-03-22 23:57:43 -0300

    servicos

---
+++ ProjetoFinal/Controllers/ServicosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoFinal.Controllers
+{
+    public class ServicosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
